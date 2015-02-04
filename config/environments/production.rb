00511cdf589cdcf7a479a835e405a3233df15cc5
MissIryna <miss.iryna@gmail.com>
2015-02-04 22:42:35 +0200

    added slider.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.precompile += %w( mobilyslider.js ) 
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 end
