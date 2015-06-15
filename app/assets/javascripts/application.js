03741e5c72a1b53538e1c398f21f8485c2086571
MissIryna <miss.iryna@gmail.com>
2015-06-15 23:19:54 +0300

    edited aplication.ja

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
 //= require bootstrap
 //= require turbolinks
-//= require jquery.fileupload
-//= require jquery.fileupload-ui
-
-
+//= require_tree .
