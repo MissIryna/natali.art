2e44a8c3383f80c769bde06955ac63f231254591
MissIryna <miss.iryna@gmail.com>
2015-06-17 16:25:07 +0300

    fsd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -72,9 +75,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.assets.precompile += %w( mobilyslider.js ) 
-  # config.assets.precompile += %w( jquery.galleriffic.js)
-  # config.assets.precompile += %w(  jquery.opacityrollover.js)
-  config.serve_static_assets = true
-  config.assets.digest = true
+  
+  config.assets.precompile += %w( mobilyslider.js ) 
+  config.assets.precompile += %w( jquery.galleriffic.js)
+  config.assets.precompile += %w(  jquery.opacityrollover.js)
+  # config.serve_static_assets = true
+ 
 end
